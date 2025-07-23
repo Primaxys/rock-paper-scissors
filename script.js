45fd73cdd9d6610365cdadf39b37f1c467c2fba3
Primaxys <214843313+Primaxys@users.noreply.github.com>
2025-07-23 14:45:25 +0800

    I've wrote the  code for the score variables of the player and computer.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//  Computer Logic
 function getComputerChoice() {
   const randomVal = Math.random();
   if (randomVal < 0.33) {
@@ -9,6 +10,7 @@ function getComputerChoice() {
   }
 }
 
+// Human Logic
 function getHumanChoice() {
   let choices = window.prompt("Rock, Paper, Or Scissors?");
 
@@ -19,6 +21,17 @@ function getHumanChoice() {
   } else if (choices == "Scissors") {
     return "Scissors";
   } else {
-    return "Please select a one of them";
+    return "Please select one of the values provided.";
   }
 }
+
+// Score Variables
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
