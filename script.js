f8bd9f9b4543e29e489ef999cb321c79c1498292
Primaxys <214843313+Primaxys@users.noreply.github.com>
2025-07-25 14:58:58 +0800

    The problem where the humanChoice param is not accepting any variation of a string like, 'RocK', or 'PaPer' has been solved by changing each string to lowercase, in order to convert it to any string variation we like through .toLowerCase (See line 15 - 57, script.js)

--- script.js
+++ script.js
@@ -1,56 +1,73 @@
-// //  Computer Logic
-// function getComputerChoice() {
-//   const randomVal = Math.random();
-//   if (randomVal < 0.33) {
-//     return "Rock";
-//   } else if (randomVal < 0.66) {
-//     return "Paper";
-//   } else {
-//     return "Scissors";
-//   }
+//  Computer Logic
+function getComputerChoice() {
+  const randomVal = Math.random();
+  if (randomVal < 0.33) {
+    return "rock";
+  } else if (randomVal < 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+// Human Logic
+function getHumanChoice() {
+  let humanChoice = window.prompt("Rock, Paper, Or Scissors?").toLowerCase();
+
+  if (humanChoice == "rock") {
+    return "rock";
+  } else if (humanChoice == "paper") {
+    return "paper";
+  } else if (humanChoice == "scissors") {
+    return "scissors";
+  } else {
+    return "Please select one of the values provided.";
+  }
+}
+
+// Score Variables
+let humanScore = 0;
+let computerScore = 0;
+
+// Round Logic
+function playRound(humanChoice, computerChoice) {
+  // Win Conditionals
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie");
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You Win! Paper beats Rock");
+    humanScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You Win! Scissors beats Paper");
+    humanScore++;
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You Win! Rock beats Scissors");
+    humanScore++;
+  }
+
+  // Lose Conditionals
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose Paper  beats Rock");
+    computerScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose Scissors beats Paper");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose Rock beats Scissors");
+    computerScore++;
+  }
+
+  console.log(humanScore);
+  console.log(computerScore);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+// function capitalize() {
+//   return String("bOTH").charAt(0).toUpperCase() + String("bOTH").slice(1);
 // }
 
-// // Human Logic
-// function getHumanChoice() {
-//   let choices = window.prompt("Rock, Paper, Or Scissors?");
-
-//   if (choices == "Rock") {
-//     return "Rock";
-//   } else if (choices == "Paper") {
-//     return "Paper";
-//   } else if (choices == "Scissors") {
-//     return "Scissors";
-//   } else {
-//     return "Please select one of the values provided.";
-//   }
-// }
-
-// // Score Variables
-// let humanScore = 0;
-// let computerScore = 0;
-
-// // Round Logic
-// function playRound(humanChoice, computerChoice) {
-//   if (humanChoice === computerChoice) {
-//     console.log("It's a tie");
-//   } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-//     console.log("You Win! Paper beats Rock");
-//   } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-//     console.log("You Win! Scissors beats Paper");
-//   } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-//     console.log("You Win! Rock beats Scissors");
-//   }
-
-//   if (humanChoice === "Rock" && computerChoice === "Paper") {
-//     console.log("You lose Paper beats Rock");
-//   } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
-//     console.log("You lose Scissors beats Paper");
-//   } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
-//     console.log("You lose Rock beats Scissors");
-//   }
-// }
-
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-
-// playRound(humanSelection, computerSelection);
+// console.log(capitalize());
